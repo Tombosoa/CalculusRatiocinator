a56065d67543a2818dc92816387e2dd160fdc153
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-07 13:15:02 +0300

    feat: first commit

---
+++ src/main/java/com/projet/calculusRatiocinator/CalculusRationcinator.java
@@ -0,0 +1,4 @@
+package com.projet.calculusRatiocinator;
+
+public class CalculusRationcinator {
+}
---
+++ src/test/java/com/projet/CalculusRatiocinator/CalculusRationcinatorTest.java
@@ -0,0 +1,9 @@
+package com.projet.CalculusRatiocinator;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CalculusRationcinatorTest {
+    @Test
+    void test(){}
+}
